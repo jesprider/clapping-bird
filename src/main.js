e44aa5eec80b2e10dea54bed88ec377ee5113920
Roman Alekseev <roman.alekseev@influencerdb.com>
2019-03-12 23:00:53 +0100

    Added pipe class.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import * as pixi from 'pixi.js';
 import getMedia from './mic';
 import Bird from './bird';
+import Pipe from './pipe';
 
 window.onload = () => {
   const app = new pixi.Application();
   document.body.appendChild(app.view);
 
   const bird = new Bird({ x: 25, y: 25, radius: 25 });
+  const pipe = new Pipe({ canvasWidth: app.view.width, canvasHeight: app.view.height });
 
   app.stage.addChild(bird.bird);
+  app.stage.addChild(pipe.pipe);
 
   let vol = 0;
 
@@ -17,6 +20,7 @@ window.onload = () => {
       bird.up();
     }
     bird.update();
+    pipe.update();
   });
 
 
---
+++ src/pipe.js
@@ -0,0 +1,47 @@
+import * as pixi from 'pixi.js';
+
+const SPEED = 2;
+const PIPE_WIDTH = 50;
+const MINIMUM_WINDOW_HEIGHT = 100;
+
+// The maximum is exclusive and the minimum is inclusive
+const getRandomInt = (min, max) => {
+  const minInt = Math.ceil(min);
+  const maxInt = Math.floor(max);
+  return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+};
+
+
+export default class Pipe {
+  constructor({ canvasHeight, canvasWidth }) {
+    const windowHeight = getRandomInt(MINIMUM_WINDOW_HEIGHT, canvasHeight / 2);
+    const windowCenter = getRandomInt(windowHeight, canvasHeight - windowHeight);
+
+    const ht = windowCenter - windowHeight / 2;
+    const hb = canvasHeight - (windowCenter + windowHeight / 2);
+    const x = canvasWidth - 50;
+    const yt = 0;
+    const yb = canvasHeight - hb;
+
+    const pipe = new pixi.Graphics();
+    pipe.lineStyle(0);
+    pipe.beginFill(0xDE3249, 1);
+    pipe.drawRoundedRect(x, yt, PIPE_WIDTH, ht, 10);
+    pipe.drawRoundedRect(x, yb, PIPE_WIDTH, hb, 10);
+    pipe.endFill();
+
+    this.pipe = pipe;
+    this.canvasWidth = canvasWidth;
+  }
+
+  update() {
+    this.pipe.x -= SPEED;
+  }
+
+  get isOffscreen() {
+    if (this.pipe.x < -this.canvasWidth) {
+      return true;
+    }
+    return false;
+  }
+}
