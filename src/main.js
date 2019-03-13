54a2f77766f643f1c8b26d6324696fbc1e4ebbc9
Roman Alekseev <roman.alekseev@influencerdb.com>
2019-03-13 16:22:19 +0100

    Added threshold sliders. Code style changes.

--- src/main.js
+++ src/main.js
@@ -4,32 +4,68 @@ import Bird from './bird';
 import Pipe from './pipe';
 
 window.onload = () => {
+  //
+  // App creation
   const app = new pixi.Application();
   document.body.appendChild(app.view);
 
+  //
+  // Sliders handling
+  const thresholdTopSlider = document.getElementById('thresholdTop');
+  const thresholdBottomSlider = document.getElementById('thresholdBottom');
+
+  let thresholdTop = thresholdTopSlider.valueAsNumber;
+  let thresholdBottom = thresholdBottomSlider.valueAsNumber;
+  let clapping = false;
+
+  thresholdTopSlider.addEventListener('input', (event) => {
+    thresholdTop = event.target.valueAsNumber;
+  });
+
+  thresholdBottomSlider.addEventListener('input', (event) => {
+    thresholdBottom = event.target.valueAsNumber;
+  });
+
+
+  //
+  // Bird creation
   const bird = new Bird({
     x: 25, y: 25, radius: 25, canvasHeight: app.view.height,
   });
-  const pipes = [];
-  let vol = 0;
-
   app.stage.addChild(bird.bird);
 
+  //
+  // Pipes
+  const pipes = [];
   setInterval(() => {
     const pipe = new Pipe({ canvasWidth: app.view.width, canvasHeight: app.view.height });
     pipes.push(pipe);
     app.stage.addChild(pipe.pipe);
   }, 2500);
 
+  //
+  // Volume handling
+  let vol = 0;
+  getMedia((data) => {
+    vol = data;
+  });
 
-  app.ticker.add((delta) => {
-    if (vol > 20) {
+  //
+  // App update callback
+  app.ticker.add(() => {
+    console.log(vol)
+    if (vol > thresholdTop && !clapping) {
       bird.up();
+      clapping = true;
+    }
+
+    if (vol < thresholdBottom) {
+      clapping = false;
     }
 
     bird.update();
 
-    for (let i = pipes.length - 1; i >= 0; i--) {
+    for (let i = pipes.length - 1; i >= 0; i -= 1) {
       pipes[i].update();
 
       if (pipes[i].isOffscreen) {
@@ -40,7 +76,4 @@ window.onload = () => {
   });
 
 
-  getMedia((data) => {
-    vol = data;
-  });
 };
